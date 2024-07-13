de264009957375801322ed4a4227de8d7da812a1
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2024-07-13 23:27:07 +0530

    deploy: added directory upload support

--- dist/fileshare/server/main.server.mjs
+++ dist/fileshare/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5UB4BZ3E.mjs";import"./chunk-NTK4E3DR.mjs";import"./chunk-XUNDFK45.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-5NW3BX6L.mjs";import"./chunk-NTK4E3DR.mjs";import"./chunk-XUNDFK45.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
