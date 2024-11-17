c77cc379365892e71d7e7cc71ade450489e24c0d
Akarsh Shrivastava <akarsh.shrivastava@soroco.com>
2024-11-18 01:28:41 +0530

    trial: updated app

--- dist/fileshare/server/main.server.mjs
+++ dist/fileshare/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MZXOTHBR.mjs";import"./chunk-QIMP5Z7P.mjs";import"./chunk-5TX3LD54.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-MGHMRTTA.mjs";import"./chunk-QIMP5Z7P.mjs";import"./chunk-5TX3LD54.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
