18e82974f4249b23846912f9b73742320ed12e7f
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2025-01-11 23:10:23 +0530

    updated chat interface

--- dist/fileshare/server/main.server.mjs
+++ dist/fileshare/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KPVRXYR5.mjs";import"./chunk-CQEV5KFZ.mjs";import"./chunk-E5YO33S7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ZP7LKHBK.mjs";import"./chunk-CQEV5KFZ.mjs";import"./chunk-E5YO33S7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { SidenavComponent } from "./sidenav/sidenav.component";
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidenavComponent, MatSidenavModule],
+  imports: [RouterOutlet, HeaderComponent, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
